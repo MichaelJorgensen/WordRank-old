369c8363c24ab52a2e7326103c1471242f594da3
Mike <dontlookbehindyou@live.com>
2011-07-14 13:03:11 -0700

    Start of WordRank class

---
+++ src/com/lala/wordrank/WordRank.java
@@ -0,0 +1,14 @@
+package com.lala.wordrank;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WordRank extends JavaPlugin{
+	
+	public void onEnable(){
+		
+		System.out.println("[WordRank] Enabled!");
+	}
+	public void onDisable(){
+		System.out.println("[WordRank] Disabled!");
+	}
+}
