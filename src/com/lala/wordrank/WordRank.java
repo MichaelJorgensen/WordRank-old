f91b2ba66b44e8dbf80f9440da3fcd9cad66a2ba
Mike <dontlookbehindyou@live.com>
2011-07-14 13:06:45 -0700

    Permissions support

--- src/com/lala/wordrank/WordRank.java
+++ src/com/lala/wordrank/WordRank.java
@@ -1,14 +1,35 @@
 package com.lala.wordrank;
 
+import java.io.File;
+
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
+
 public class WordRank extends JavaPlugin{
-	
+	public static PermissionHandler permissionHandler;
+	public static File data;
 	public void onEnable(){
-		
+		data = getDataFolder();
+		setupPermissions();
 		System.out.println("[WordRank] Enabled!");
 	}
 	public void onDisable(){
 		System.out.println("[WordRank] Disabled!");
+	}
+	private void setupPermissions() {
+	    if (permissionHandler != null) {
+	        return;
+	    }	    
+	    Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");	    
+	    if (permissionsPlugin == null) {
+	        System.out.println("Permissions not detected, disabling HarderMob!");
+	        Plugin plugin = this.getServer().getPluginManager().getPlugin("HarderMob");
+	        this.getServer().getPluginManager().disablePlugin(plugin);	        
+	        return;
+	    }
+	    permissionHandler = ((Permissions) permissionsPlugin).getHandler();	    
 	}
 }
